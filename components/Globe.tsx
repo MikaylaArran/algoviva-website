5ca1810804a70955ff45cb772fba75af24333035
“MikaylaArran” <mikaylaarran16@gmail.com>
2026-04-06 17:13:17 +0200

    White hero, light globe

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -15,14 +15,14 @@ export default function Globe() {
       width: 800,
       height: 800,
       phi: 0.35,
-      theta: 0.2,
-      dark: 1,
-      diffuse: 1.4,
-      mapSamples: 20000,
-      mapBrightness: 6,
-      baseColor: [0.1, 0.15, 0.35],
+      theta: 0.15,
+      dark: 0,
+      diffuse: 1.2,
+      mapSamples: 24000,
+      mapBrightness: 8,
+      baseColor: [0.9, 0.92, 0.98],
       markerColor: [0.16, 0.25, 0.58],
-      glowColor: [0.16, 0.25, 0.58],
+      glowColor: [0.7, 0.78, 0.95],
       markers: [
         { location: [-26.2, 28.0], size: 0.08 },
         { location: [-33.9, 18.4], size: 0.05 },
@@ -37,16 +37,8 @@ export default function Globe() {
   }, [])
 
   return (
-    <div style={{
-      width: '100%',
-      maxWidth: 560,
-      margin: '0 auto',
-      aspectRatio: '1',
-    }}>
-      <canvas
-        ref={canvasRef}
-        style={{ width: '100%', height: '100%' }}
-      />
+    <div style={{ width: '100%', maxWidth: 560, margin: '0 auto', aspectRatio: '1' }}>
+      <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />
     </div>
   )
 }
